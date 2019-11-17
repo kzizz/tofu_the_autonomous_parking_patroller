e89960bb349744d411163b71cb61b9c1bd3b5a26
fizzer <fizzer@enph353.JoDo>
2019-11-16 21:44:29 -0800

    Image processing started to isolate for parking number and license

--- src/imgProcessing.py
+++ src/imgProcessing.py
@@ -5,26 +5,29 @@ from __future__ import print_function
 import roslib
 import numpy as np
 import math
-#roslib.load_manifest('my_package')
 import sys
 import rospy
 from geometry_msgs.msg import Twist
 import matplotlib.pyplot as plt
 import cv2
+import matplotlib.patches as patches
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
- 
+from skimage.io import imread
+from skimage.filters import threshold_otsu
+from skimage import measure
+from skimage.measure import regionprops
 
-
-class robot_controller:
+class img_processor:
 
     def __init__(self):
+        #Publishing and subscribing
         self.image_pub = rospy.Publisher("image_topic_2",Image)
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
-        self.velocity_cmd = rospy.Publisher('/R1/cmd_vel', Twist,queue_size=1)
-        self.targetOffset = 450
+        #CHANGE this to publishing to CNN:
+        #self.velocity_cmd = rospy.Publisher('/R1/cmd_vel', Twist,queue_size=1)
 
     def callback(self,data):
         try:
@@ -35,30 +38,16 @@ class robot_controller:
 
         IMAGE_H = rows
         IMAGE_W = cols
-
-        # #birdeye view of the image
-        src = np.float32([[0, IMAGE_H], [1207, IMAGE_H], [0, 0], [IMAGE_W, 0]])
-        dst = np.float32([[569, IMAGE_H], [711, IMAGE_H], [0, 0], [IMAGE_W, 0]])
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-        Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
-
-    
-        cv_warpedReady = cv_image[450:(450+IMAGE_H), 0:IMAGE_W] # Apply np slicing for ROI crop
-        birdseye_img = cv2.warpPerspective(cv_warpedReady, M, (IMAGE_W, IMAGE_H)) # Image warping
-        warped_img = cv_image[rows-200:, cols-400:cols] #CHANGE 
-        #color masks 
-        #detecting lines on the street
-        lowerWhite = np.array([250, 250, 250],dtype = "uint8")
-        upperWhite = np.array([255, 255, 255],dtype = "uint8")
+        #crop image to probable height of license plate
+        warped_img = cv_image[rows-400:cols-400]
+        
+        #color masks
+        # For detecting license plates 
+        lowerWhite = np.array([100, 100, 100],dtype = "uint8")
+        upperWhite = np.array([200, 200, 200],dtype = "uint8")
         whiteMask = cv2.inRange(warped_img, lowerWhite, upperWhite)
-        # whiteMask = cv2.blur(whiteMask, (5,5))
         whiteMask = cv2.medianBlur(whiteMask, 5)
         whiteMask = cv2.erode(whiteMask, None, iterations=2)
-	# whiteMask = cv2.dilate(whiteMask, None, iterations=2)
-        #detecting the street
-        lowerGray = np.array([50, 80, 50],dtype = "uint8")
-        upperGray = np.array([190, 90, 90],dtype = "uint8")
-        grayMask = cv2.inRange(birdseye_img, lowerGray, upperGray)
         #grass green
         lowerGreen = np.array([10,70,10],dtype = "uint8")
         upperGreen = np.array([70,210,30],dtype = "uint8")
@@ -67,79 +56,90 @@ class robot_controller:
         lowerRed = np.array([0, 0, 255-20],dtype = "uint8")
         upperRed = np.array([255, 20, 255],dtype = "uint8")
         redMask = cv2.inRange(warped_img, lowerRed, upperRed)
-        #blue for car detection
-        lowerBlue = np.array([0, 0, 0],dtype = "uint8")
-        upperBlue = np.array([255, 30, 20],dtype = "uint8")
+        #blue for car detection TODO: Adjust values to exclude background!
+        lowerBlue = np.array([0, 0, 0],dtype = "uint8") #OG 0 0 0 
+        upperBlue = np.array([255, 30, 20],dtype = "uint8") #OG 255, 30,20
         blueMask = cv2.inRange(warped_img, lowerBlue, upperBlue)
         #apply masks for lane detection
         greenOutput = cv2.bitwise_and(warped_img, warped_img, mask = greenMask)
         redOutput = cv2.bitwise_and(warped_img, warped_img, mask = redMask)
-        grayOutput = cv2.bitwise_and(birdseye_img, birdseye_img, mask = grayMask)
         whiteOutput = cv2.bitwise_and(warped_img, warped_img, mask = whiteMask)
         blueOutput = cv2.bitwise_and(warped_img, warped_img, mask = blueMask)
 
-        grayWarped = cv2.cvtColor(whiteOutput,cv2.COLOR_BGR2GRAY)
-        ret,thresh = cv2.threshold(grayWarped, 20, 255, 0)
-        img, contours, hierarchy = cv2.findContours(thresh, 1, 2)
-        #find center of mass
-        M = cv2.moments(img)
-        offset = 0
-        middlePixel = 0
-        cX = 0
-        cY = 0
-        if(M["m00"] == 0):
-            offset = self.targetOffset
-        else:
-            cX = cols - 400 + int(M["m10"]/M["m00"])
-            cY = rows - 200 + int(M["m01"]/ M["m00"])
-            middlePixel = cols/2
-            offset = cX - middlePixel
-            print("current offset:")
-            print(offset)
-            print("target offset: ")
-            print(self.targetOffset)
-        cv2.circle(cv_image,(middlePixel,cY), 5, (255,0,0))
-        cv2.circle(cv_image, (cX,cY), 5, (255,0,0))
-        cv2.imshow("whiteMask",whiteOutput)
+        blueBinary = self.make_binary_image(blueOutput)
+        carCroppedImg=self.crop_image_only_outside_using_mask(blueBinary,warped_img,tol=0)
+
+        cv2.imshow("cropped",carCroppedImg)
         cv2.waitKey(3)
-        cv2.imshow("Image window", warped_img)
-        cv2.waitKey(3)
-        cv2.imshow("contour image",birdseye_img)
+        cv2.imshow("bluebin",blueBinary)
+        cv2.waitKey(3) 
+        cv2.imshow("blue",blueOutput)
         cv2.waitKey(3) 
 
-        grayPercentage = float(np.count_nonzero(np.asarray(grayOutput)))/float(np.count_nonzero(np.asarray(birdseye_img)))
-        print(grayPercentage)
+        #self.boundary_finder(grayCarImg,binaryCarImg)
 
-        self.pid(offset)
+    
+    #find the boundaries of the license plate using connected component analysis
+    def boundary_finder(self,grayImg,binaryImg):
+        #Find connected components
+        labelImg = measure.label(binaryImg)
 
-    def pid(self,offset):
-        # slopeThresh = 5000
-        differenceTolerance = 55
-        angularScale = 5
-        xVelocity = 0.03
-        zTwist = 0.0
-        offsetOvershoot = self.targetOffset - offset
-        print("offset overshoot")
-        print(offsetOvershoot )
-        if(abs(offsetOvershoot) > differenceTolerance):
-            print("turning")
-            if(offsetOvershoot > 0):
-                print ("right")
-            else:
-                print("left")
-            xVelocity = 0.0
-            zTwist = angularScale * offsetOvershoot
-        vel_msg = Twist()
-        vel_msg.linear.x = xVelocity
-        vel_msg.angular.z = zTwist
-        # print (vel_msg)
-        self.velocity_cmd.publish(vel_msg)
-        
+        #Find the expected dimmensions of a license plate
+        plate_dimensions = (0.3*labelImg.shape[0], 0.6*labelImg.shape[0], 0.04*labelImg.shape[1], 0.12*labelImg.shape[1])
+        min_height, max_height, min_width, max_width = plate_dimensions
+        plate_objects_cordinates = []
+        plate_like_objects = []
+
+        # regionprops creates a list of properties of all the labelled regions
+        for region in regionprops(labelImg):
+            if region.area < 100:
+            #if the region is so small then it's likely not a license plate
+                continue
+
+            ##FOLLOWING IS NON-RESTRICTIVE METHOD
+            min_row, min_col, max_row, max_col = region.bbox
+            rectBorder = cv2.rectangle(grayImg, (min_col, min_row), (max_col, max_row), (255,0,0), 2)
+            print("I found a rectangle!")
+
+            # ##FOLLOWING IS MORE RESTRICTIVE METHOD
+            # # the bounding box coordinates
+            # min_row, min_col, max_row, max_col = region.bbox
+            # region_height = max_row - min_row
+            # region_width = max_col - min_col
+            # # ensuring that the region identified satisfies the condition of a typical license plate
+            # if region_height >= min_height and region_height <= max_height and region_width >= min_width and region_width <= max_width and region_width > region_height:
+            #     plate_like_objects.append(binaryImg[min_row:max_row,min_col:max_col])
+            #     plate_objects_cordinates.append((min_row, min_col,max_row, max_col))
+            #     cv2.rectangle(grayImg, (min_col, min_row), (max_col, max_row), (0,255,0), 2)
+            #     print("I found a rectangle!")
+        cv2.imshow("binary",binaryImg)
+        cv2.waitKey(3) 
+        print(binaryImg)
+        cv2.imshow("rectangles",grayImg)
+        cv2.waitKey(3) 
+
+    def crop_image_only_outside_using_mask(self,des_mask,img,tol=0):
+        # img is 2D image data
+        # tol  is tolerance
+        mask = des_mask>tol
+        print (des_mask.shape)
+        m,n = des_mask.shape
+        mask0,mask1 = mask.any(0),mask.any(1)
+        col_start,col_end = mask0.argmax(),n-mask0[::-1].argmax()
+        row_start,row_end = mask1.argmax(),m-mask1[::-1].argmax()
+        return img[row_start:row_end,col_start:col_end]
+
+    def make_binary_image(self,img):
+        #turn image into grayscale and binary
+        grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        threshold_value = threshold_otsu(grayImg)
+        binaryImg = cv2.threshold(grayImg, threshold_value, 255, cv2.THRESH_BINARY)[1]
+        return binaryImg
 
 
 def main(args):
-    rospy.init_node('robot_controller', anonymous=True)
-    rc = robot_controller()
+    rospy.init_node('img_processor', anonymous=True) #CHECK : Does this needed to be added to a world.launch file somewhere?
+    imgP = img_processor()
     # try:
     rospy.spin()
     # except KeyboardInterrupt:
